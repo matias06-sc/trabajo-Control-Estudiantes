e1427df8c7ce64ffadd5c1b058c1a10944ef9973
rodrixd123 <communityeren@gmail.com>
2025-08-16 11:55:56 -0500

    feat: implementación inicial de CRUD de tareas con conexión a MySQL y persistencia de datos en interfaz

--- control/estudiantes/models.py
+++ control/estudiantes/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=100)
     carrera = models.CharField(max_length=100)
-    ciclo = models.IntegerField()
-    correo = models.EmailField()
+    ciclo = models.CharField(max_length=20)
+    correo = models.EmailField(unique=True)
 
     def __str__(self):
         return self.nombre
--- control/estudiantes/views.py
+++ control/estudiantes/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Estudiante
 
-# Create your views here.
+# Lista todos los estudiantes
+def lista_estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'estudiantes/lista.html', {'estudiantes': estudiantes})
+
+# Crear un estudiante
+def crear_estudiante(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        carrera = request.POST['carrera']
+        ciclo = request.POST['ciclo']
+        correo = request.POST['correo']
+        Estudiante.objects.create(nombre=nombre, carrera=carrera, ciclo=ciclo, correo=correo)
+        return redirect('lista_estudiantes')
+    return render(request, 'estudiantes/lista.html')   # 👈 corregido
+
+# Editar estudiante
+def editar_estudiante(request, id):
+    estudiante = get_object_or_404(Estudiante, id=id)
+    if request.method == 'POST':
+        estudiante.nombre = request.POST['nombre']
+        estudiante.carrera = request.POST['carrera']
+        estudiante.ciclo = request.POST['ciclo']
+        estudiante.correo = request.POST['correo']
+        estudiante.save()
+        return redirect('lista_estudiantes')
+    return render(request, 'estudiantes/lista.html', {'estudiante': estudiante})
+
+# Eliminar estudiante
+def eliminar_estudiante(request, id):
+    estudiante = get_object_or_404(Estudiante, id=id)
+    estudiante.delete()
+    return redirect('lista_estudiantes')
